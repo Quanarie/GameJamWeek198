561ba34df5158a22c35d2fd27aa3a339abbc0590
llNovall <25434267+llNovall@users.noreply.github.com>
2021-04-24 19:20:13 +0200

    Added script for initializing passive birds

---
+++ Assets/Scripts/Bird/BirdPassiveInitializer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is for initializing aggressive birds
+/// </summary>
+public class BirdPassiveInitializer : BirdInitializer
+{
+
+}

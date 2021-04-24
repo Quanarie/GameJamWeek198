d96f290a064aac1171661fdc34174c5f16aa1aef
llNovall <25434267+llNovall@users.noreply.github.com>
2021-04-24 21:58:23 +0200

    Updated BirdSpawnerInfo which now keeps track of bird's health as well before adding to spawner pool

--- Assets/Scripts/Bird/BirdSpawnerInfo.cs
+++ Assets/Scripts/Bird/BirdSpawnerInfo.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 /// <summary>
 /// This class is used to keep track of the Spawner that created this object.
-/// When this object becomes inivisible, it will add itself to the object pool of the spawner.
+/// When this object becomes invisible or dead, it will add itself to the object pool of the spawner.
 /// </summary>
 public class BirdSpawnerInfo : MonoBehaviour
 {
     [SerializeField]
     private BirdSpawner _parentSpawner;
 
+    private void Awake()
+    {
+        BirdHealth birdHealth = gameObject.GetComponent<BirdHealth>();
+        if (birdHealth)
+        {
+            birdHealth.SubscribeToOnDeath(BirdHealth_OnDeath);
+        }
+        else
+            Debug.LogError($"{GetType().FullName} : Failed to find BirdHealth");
+    }
+
     public void Initialize(BirdSpawner birdSpawner)
     {
         _parentSpawner = birdSpawner;
+    }
+
+    /// <summary>
+    /// If bird is dead, it disables gameobject. If not, it enables the gameobject.
+    /// </summary>
+    /// <param name="isDead"></param>
+    private void BirdHealth_OnDeath(bool isDead)
+    {
+        gameObject.SetActive(!isDead);
+
+        if(isDead)
+            _parentSpawner.AddObjectToObjectPool(gameObject);
     }
 
     private void OnBecameInvisible()

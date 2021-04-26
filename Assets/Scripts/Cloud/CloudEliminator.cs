3c982c42ea9f0c4230e0eb1f31515c37f582b796
llNovall <25434267+llNovall@users.noreply.github.com>
2021-04-26 05:02:38 +0200

    Updated CloudEliminator
    CloudEliminator used to stay on an object and removed all the clouds. The new change made it to be attached to the cloud itself. When the cloud is out of range, the cloud is destroyed.

--- Assets/Scripts/Cloud/CloudEliminator.cs
+++ Assets/Scripts/Cloud/CloudEliminator.cs
@@ -5,26 +5,11 @@ using UnityEngine;
 public class CloudEliminator : MonoBehaviour
 {
     /// <summary>
-    /// This script deletes all clouds from CloudSpawner.Clouds that are out of camera range
+    /// This script deletes the cloud if it is outside camera view
     /// </summary>
-    
-    [Header("Set in Inspector")]
-    public float maxYCoordinate;
 
-    void Update()
+    private void OnBecameInvisible()
     {
-        DeleteClouds();
-    }
-    void DeleteClouds()
-    {
-        List<GameObject> Clouds = GetComponentInParent<CloudSpawner>().Clouds;
-        for (int i = 0; i < Clouds.Count; i++)
-        {
-            if (Clouds[i].transform.position.y > maxYCoordinate)
-            {
-                Destroy(Clouds[i]);
-                Clouds.Remove(Clouds[i]);
-            }
-        }
+        Destroy(gameObject);
     }
 }

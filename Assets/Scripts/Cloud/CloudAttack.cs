b83d250d9e40d6a6896bdc08f3769927671e0f26
llNovall <25434267+llNovall@users.noreply.github.com>
2021-04-27 00:21:20 +0200

    Hotfix for CloudAttack behavior

--- Assets/Scripts/Cloud/CloudAttack.cs
+++ Assets/Scripts/Cloud/CloudAttack.cs
@@ -7,12 +7,27 @@ public class CloudAttack : MonoBehaviour
     [SerializeField]
     private float _attackDamage;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (PlayerIdentifier.Current.gameObject == collision.gameObject)
+    //    {
+    //        CatHealth catHealth = PlayerIdentifier.Current.GetComponent<CatHealth>();
+    //        catHealth.ReduceHealth(_attackDamage);
+    //        Debug.LogError($"Collider");
+    //    }
+    //}
+
+    /// <summary>
+    /// This is a temporary fix for cloud attack. This function should be removed at a later stage to respect cat's attack range trigger and cat's collider
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(PlayerIdentifier.Current == collision.gameObject)
+        if (PlayerIdentifier.Current.gameObject == collision.gameObject)
         {
             CatHealth catHealth = PlayerIdentifier.Current.GetComponent<CatHealth>();
             catHealth.ReduceHealth(_attackDamage);
+            Debug.LogError($"Trigger");
         }
     }
 }

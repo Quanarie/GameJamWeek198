bd99b3f881de1098ce78941dc631cedf8235e420
sppeeedd <qustaine@gmail.com>
2021-04-26 10:30:14 +0300

    Improved catAttack script. Added a tag to the passive birds so that cat can eat them.

--- Assets/Scripts/Cat/CatAttack.cs
+++ Assets/Scripts/Cat/CatAttack.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class CatAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Set in Inspector")]
+    public float attackRadius;
+    public LayerMask birdLayerMask;
+
+    [SerializeField]
+    private Parachute _parachute;
+    private ParachuteMode _parachuteMode;
+
     void Start()
     {
-        
+        if (!_parachute)
+        {
+            _parachute = GetComponent<Parachute>();
+            if (!_parachute)
+            {
+                _parachute = GetComponentInChildren<Parachute>();
+                _parachute.SubscribeToOnParachuteModeChanged(Parachute_OnParachuteModeChanged);
+            }
+
+            else
+                Debug.LogError($"{GetType().FullName} : Failed to find Parachute.");
+        }
+        _parachute.SubscribeToOnParachuteModeChanged(Parachute_OnParachuteModeChanged);
+    }
+    void FixedUpdate()
+    {
+        if (_parachuteMode == ParachuteMode.Close && Input.GetKey(KeyCode.S))
+        {
+            GameObject target = FindClosestBirdUnderCat();
+            if (target != null)
+            {
+                Destroy(target);
+                
+                //add health
+            }
+        }
+    }
+    GameObject FindClosestBirdUnderCat()
+    {
+        GameObject[] birds = GameObject.FindGameObjectsWithTag("Bird");
+        GameObject closestBird = null;
+        foreach (GameObject bird in birds)
+        {
+            Vector3 catPos = transform.position;
+            Vector3 birdPos = bird.transform.position;
+            if (catPos.y - birdPos.y < attackRadius && catPos.y - birdPos.y > 0 && Mathf.Abs(catPos.x - birdPos.x) < attackRadius)
+            {
+                if (closestBird != null)
+                {
+                    if (Mathf.Abs(birdPos.y - catPos.y) < Mathf.Abs(closestBird.transform.position.y - catPos.y))
+                    {
+                        closestBird = bird;
+                    }
+                }
+                else
+                {
+                    closestBird = bird;
+                }
+            }
+        }
+        return closestBird;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private void Parachute_OnParachuteModeChanged(ParachuteMode parachuteMode) => _parachuteMode = parachuteMode;
 }

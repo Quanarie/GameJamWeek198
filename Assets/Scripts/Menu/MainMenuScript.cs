f4451de882a82229ba79c512b3f231481091a7f7
sppeeedd <qustaine@gmail.com>
2021-04-27 21:39:55 +0300

    Added startScene animation. Fixed bugs

--- Assets/Scripts/Menu/MainMenuScript.cs
+++ Assets/Scripts/Menu/MainMenuScript.cs
@@ -2,13 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
     public void QuitGame()
     {
         Application.Quit();

d253899a01c6360daa4ed4010380d166be34e77c
llNovall <25434267+llNovall@users.noreply.github.com>
2021-04-28 16:33:17 +0200

    Updated CatHealthUI which can now create life objects based on information from CatHealth

--- Assets/Scripts/Cat/CatHealthUI.cs
+++ Assets/Scripts/Cat/CatHealthUI.cs
@@ -8,23 +8,45 @@ public class CatHealthUI : MonoBehaviour
     [SerializeField]
     private List<GameObject> _objLives = new List<GameObject>();
 
+    [SerializeField]
+    private GameObject _objLife;
+
+    [SerializeField]
+    private Transform _parent;
     private void Start()
     {
         if (PlayerIdentifier.Current)
         {
             CatHealth catHealth = PlayerIdentifier.Current.GetComponent<CatHealth>();
 
-            catHealth.SubscribeToOnDamageTaken(CatHealth_OnDamageTaken);
+            catHealth.SubscribeToOnDamageTaken(CatHealth_OnHealthChanged);
+            catHealth.SubscribeToOnHealthGained(CatHealth_OnHealthChanged);
+            CreateLifeObjects(catHealth.GetCurrentHealth());
         }
     }
 
-    private void CatHealth_OnDamageTaken()
+    private void CreateLifeObjects(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            GameObject objLife = Instantiate(_objLife, _parent);
+            objLife.SetActive(true);
+            _objLives.Add(objLife);
+        }
+    }
+    private void CatHealth_OnHealthChanged(int currentHealth)
     {
         if(_objLives.Count > 0)
         {
-            GameObject objLife = _objLives[Mathf.Min(0, _objLives.Count - 1)];
-            objLife.SetActive(false);
-            _objLives.Remove(objLife);
+            for (int i = 0; i < _objLives.Count; i++)
+            {
+                if (i <= currentHealth - 1)
+                {
+                    _objLives[i].SetActive(true);
+                }
+                else
+                    _objLives[i].SetActive(false);
+            }
         }
         
     }

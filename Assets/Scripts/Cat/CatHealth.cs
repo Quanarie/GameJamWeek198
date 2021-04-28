81057f19535d1c1078a30234ae12305e559e8fb0
llNovall <25434267+llNovall@users.noreply.github.com>
2021-04-28 21:10:58 +0200

    Updated CatHealth

--- Assets/Scripts/Cat/CatHealth.cs
+++ Assets/Scripts/Cat/CatHealth.cs
@@ -6,27 +6,49 @@ using UnityEngine.Events;
 public class CatHealth : MonoBehaviour
 {
     [SerializeField]
-    private int _maxHealth;
+    private int _maxNumOfLives;
+    [SerializeField]
+    private int _currentNumOfLives;
+    [SerializeField]
+    private int _maxHealthPerLife;
 
     [SerializeField]
     private int _currentHealth;
 
     private event UnityAction<bool> OnDeath;
-    private event UnityAction<int> OnDamageTaken;
-    private event UnityAction<int> OnHealthGained;
+
+    /// <summary>
+    /// First value is number of remaining life. Second value is remaining health for the current life.
+    /// </summary>
+    private event UnityAction<int, int> OnDamageTaken;
+    /// <summary>
+    /// First value is number of remaining life. Second value is remaining health for the current life.
+    /// </summary>
+    private event UnityAction<int, int> OnHealthGained;
+
+    private event UnityAction OnHealthReset;
 
 
     private void Awake()
     {
-        _currentHealth = _maxHealth;
+        _currentHealth = _maxHealthPerLife;
+        _currentNumOfLives = _maxNumOfLives;
     }
 
-    public int GetMaxHealth() => _maxHealth;
+    public void ResetCatHealth()
+    {
+        _currentNumOfLives = _maxNumOfLives;
+        _currentHealth = _maxHealthPerLife;
+        OnHealthReset?.Invoke();
+    }
+    public int GetMaxHealth() => _maxHealthPerLife;
     public int GetCurrentHealth() => _currentHealth;
+    public int GetMaxLife() => _maxNumOfLives;
+    public int GetCurrentLife() => _currentNumOfLives;
     public void AddHealth(int healthToAdd)
     {
-        _currentHealth = Mathf.Min(_currentHealth + healthToAdd, _maxHealth);
-        OnHealthGained?.Invoke(_currentHealth);
+        _currentHealth = Mathf.Min(_currentHealth + healthToAdd, _maxHealthPerLife);
+        OnHealthGained?.Invoke(_currentNumOfLives, _currentHealth);
     }
 
     /// <summary>
@@ -35,12 +57,19 @@ public class CatHealth : MonoBehaviour
     /// <param name="healthToReduce"></param>
     public void ReduceHealth(int healthToReduce)
     {
-        _currentHealth = Mathf.Clamp(_currentHealth - healthToReduce, 0, _maxHealth);
-
-        OnDamageTaken?.Invoke(_currentHealth);
-
-        if (_currentHealth == 0)
+        _currentHealth = Mathf.Clamp(_currentHealth - healthToReduce, 0, _maxHealthPerLife);
+        OnDamageTaken?.Invoke(_currentNumOfLives, _currentHealth);
+        if(_currentHealth == 0)
         {
+            _currentHealth = _maxHealthPerLife;
+            _currentNumOfLives = Mathf.Max(_currentNumOfLives - 1, 0);
+        }
+
+        OnDamageTaken?.Invoke(_currentNumOfLives, _currentHealth);
+
+        if (_currentNumOfLives == 0)
+        {
+            Debug.LogError("CAT IS DEAD");
             OnDeath?.Invoke(true);
         }
     }
@@ -48,11 +77,14 @@ public class CatHealth : MonoBehaviour
     #region Subscriptions
     public void SubscribeToOnDeath(UnityAction<bool> callback) => HelperUtility.SubscribeTo(ref OnDeath, ref callback);
     public void UnsubscribeFromOnDeath(UnityAction<bool> callback) => HelperUtility.UnsubscribeFrom(ref OnDeath, ref callback);
-    public void SubscribeToOnDamageTaken(UnityAction<int> callback) => HelperUtility.SubscribeTo(ref OnDamageTaken, ref callback);
-    public void UnsubscribeFromOnDamageTaken(UnityAction<int> callback) => HelperUtility.UnsubscribeFrom(ref OnDamageTaken, ref callback);
+    public void SubscribeToOnDamageTaken(UnityAction<int,int> callback) => HelperUtility.SubscribeTo(ref OnDamageTaken, ref callback);
+    public void UnsubscribeFromOnDamageTaken(UnityAction<int,int> callback) => HelperUtility.UnsubscribeFrom(ref OnDamageTaken, ref callback);
 
-    public void SubscribeToOnHealthGained(UnityAction<int> callback) => HelperUtility.SubscribeTo(ref OnDamageTaken, ref callback);
-    public void UnsubscribeFromOnHealthGained(UnityAction<int> callback) => HelperUtility.UnsubscribeFrom(ref OnDamageTaken, ref callback);
+    public void SubscribeToOnHealthGained(UnityAction<int,int> callback) => HelperUtility.SubscribeTo(ref OnHealthGained, ref callback);
+    public void UnsubscribeFromOnHealthGained(UnityAction<int,int> callback) => HelperUtility.UnsubscribeFrom(ref OnHealthGained, ref callback);
+    public void SubscribeToOnHealthReset(UnityAction callback) => HelperUtility.SubscribeTo(ref OnHealthReset, ref callback);
+    public void UnsubscribeFromOnHealthReset(UnityAction callback) => HelperUtility.UnsubscribeFrom(ref OnHealthReset, ref callback);
+
 
     #endregion
 }

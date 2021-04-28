21e56f3e1765e75a13823c01fdf2530a6a41695f
llNovall <25434267+llNovall@users.noreply.github.com>
2021-04-28 21:09:38 +0200

    Updated CloudSound which now won't play idle sound

--- Assets/Scripts/Cloud/CloudSound.cs
+++ Assets/Scripts/Cloud/CloudSound.cs
@@ -8,15 +8,7 @@ using UnityEngine;
 public class CloudSound : SoundPlayer
 {
     [SerializeField]
-    private string _cloudIdleSound;
-
-    [SerializeField]
     private string _cloudAttackSound;
-
-    [SerializeField]
-    private float _volumeIdle;
-
-    private EventInstance _eventInstance;
 
     private void Start()
     {
@@ -27,68 +19,9 @@ public class CloudSound : SoundPlayer
         }
         else
             Debug.LogError($"{GetType().FullName} : Failed to find Cloud Attack.");
-
-        if (VolumeManager.Current)
-        {
-            _volumeIdle = VolumeManager.Current.SFX;
-            VolumeManager.Current.SubscribeToChangeSFX(VolumeManager_OnChangeSFX);
-        }
-        else
-            Debug.LogError($"Failed to find VolumeManager");
-    }
-
-    private void PlayCloudIdleSound()
-    {
-        if (_eventInstance.isValid())
-        {
-            _eventInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-            _eventInstance.release();
-        }
-
-        _eventInstance = RuntimeManager.CreateInstance(_cloudIdleSound);
-        _eventInstance.start();
-        _eventInstance.setVolume(_volumeIdle);
-    }
-
-    private void StopCloudIdleSound()
-    {
-        if (_eventInstance.isValid())
-        {
-            _eventInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-            _eventInstance.release();
-        }
     }
     private void CloudAttack_OnCloudAttack()
     {
         PlaySound(_cloudAttackSound);
     }
-    private void UpdateVolume(float newVolume)
-    {
-        _volumeIdle = newVolume;
-        if (_eventInstance.isValid())
-        {
-            _eventInstance.setVolume(_volumeIdle);
-        }
-    }
-
-    private void OnBecameVisible()
-    {
-        PlayCloudIdleSound();
-    }
-
-    protected void OnBecameInvisible()
-    {
-        StopCloudIdleSound();
-    }
-    private void OnDestroy()
-    {
-        if (VolumeManager.Current)
-        {
-            VolumeManager.Current.UnsubscribeFromChangeSFX(VolumeManager_OnChangeSFX);
-        }
-        else
-            Debug.LogError($"Failed to find VolumeManager");
-    }
-
-    private void VolumeManager_OnChangeSFX(float volume) => UpdateVolume(volume);
 }

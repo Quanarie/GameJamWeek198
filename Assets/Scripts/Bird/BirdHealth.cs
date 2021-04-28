a04dbb040cc7c67eefbc799c13fe0cf8d1278831
llNovall <25434267+llNovall@users.noreply.github.com>
2021-04-28 16:27:27 +0200

    Updated Bird Health which will give a health to cat when it dies

--- Assets/Scripts/Bird/BirdHealth.cs
+++ Assets/Scripts/Bird/BirdHealth.cs
@@ -14,11 +14,27 @@ public class BirdHealth : MonoBehaviour
     [SerializeField]
     private float _currentHealth;
 
+    [SerializeField]
+    private CatHealth _catHealth;
+
     private event UnityAction<bool> OnDeath;
+
     public void Initialize(BirdHealthData birdHealthData)
     {
         _maxHealth = birdHealthData.MaxHealth;
         _currentHealth = _maxHealth;
+
+        if (!_catHealth)
+        {
+            if (PlayerIdentifier.Current)
+            {
+                _catHealth = PlayerIdentifier.Current.GetComponent<CatHealth>();
+                if (!_catHealth)
+                    Debug.LogError($"{GetType().FullName} : Failed to find CatHealth.");
+            }
+            else
+                Debug.LogError($"{GetType().FullName} : Failed to find PlayerIdentifier.");
+        }
         OnDeath?.Invoke(false);
     }
 
@@ -34,6 +50,7 @@ public class BirdHealth : MonoBehaviour
         {
             //Probably a dirty way to get a kill counter. I will change this when I get time
             PlayerBirdKillTracker.Current.AddKillCount(1);
+            _catHealth.AddHealth(1);
             if(PlayerBirdKillTracker.Current)
             OnDeath?.Invoke(true);
         }
